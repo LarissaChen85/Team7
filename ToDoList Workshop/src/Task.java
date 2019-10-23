6ec60c2c8ea082e772b8b581b5fc98f2777d45d2
dtan <dtan@172.17.221.100>
2019-10-23 16:33:14 +0800

    Addtask works

--- ToDoList Workshop/src/Task.java
+++ ToDoList Workshop/src/Task.java
@@ -1,7 +1,12 @@
+import java.util.*;
+import java.time.LocalDate;
+import java.time.temporal.*;
 
 public class Task {
 	private String description = null;
 	private boolean isComplete = false;
+	private LocalDate duedate = null;
+	private int notification = 0;
 	
 	
 	public Task(String description) {
@@ -14,7 +19,15 @@ public class Task {
 		this.description = description;
 		this.isComplete = isComplete;
 	}
-	
+
+	public Task(String description, boolean isComplete, LocalDate duedate, int notification) {
+		super();
+		this.description = description;
+		this.isComplete = isComplete;
+		this.duedate = duedate;
+		this.notification = notification;
+	}
+
 	public String getDescription() {
 		return description;
 	}
@@ -27,5 +40,70 @@ public class Task {
 	public void setComplete(boolean isComplete) {
 		this.isComplete = isComplete;
 	}
+	
+	public LocalDate getDueDate() {
+		return duedate;
+	}
+	
+	public void setDueDate(LocalDate duedate) {
+		this.duedate = duedate;
+	}
+	
+	public int getNotification() {
+		return notification;
+	}
+	
+	public String checkNotification(Task task) {
+		String desc;
+		boolean status;
+		LocalDate currentDate = LocalDate.now();
+		desc = task.getDescription();
+		duedate = task.getDueDate();
+		notification = task.getNotification();
+		status = task.isComplete();
+		System.out.println(currentDate);
+		System.out.println(duedate);
+	
+		if (duedate.isAfter(currentDate) || duedate.isEqual(currentDate))
+		{
 
+			if (duedate.isEqual(currentDate))
+			{
+				System.out.println(notification);
+				if(notification == 0) {
+					System.out.println("Task " + desc + ": Due today. ");
+					return "Task " + desc + ": Due today. ";  
+				}
+				else {
+					System.out.println("Task " + desc + ": Notification date is invalid! ");
+					return "Task " + desc + ": Notification date is invalid! ";  
+				}
+			}
+			else
+			{
+				long days = currentDate.until(duedate, ChronoUnit.DAYS);
+				if (days == notification) {
+					int daysBetween = (int)days - notification;
+					String daysBetweenStr = Integer.toString(daysBetween);
+					
+					System.out.println("Task " + desc + ": " + daysBetweenStr + " days left.");
+					return "Task " + desc + ": " + daysBetweenStr + " days left.";
+				}
+				else if (days > notification) {
+					System.out.println("Task " + desc + ": Take your time! ");
+					return "Task " + desc + ": Take your time! ";   
+				}
+				
+				else {
+					System.out.println("Task " + desc + ": System error! :) ");
+					return "Task " + desc + ": System error! :) ";   									
+				}			
+			}
+		}
+		
+		else {
+			System.out.println("Task " + desc + ": Already overdue! ");
+			return "Task " + desc + ": Already overdue! ";  			
+		}
+	}
 }
--- ToDoList Workshop/test/ToDoListTest.java
+++ ToDoList Workshop/test/ToDoListTest.java
@@ -1,8 +1,9 @@
-import java.util.Collection;
-
+import java.util.*;
 import org.junit.*;
 import org.junit.Test;
 import junit.framework.*;
+import java.time.LocalDate;
+
 
 
 public class ToDoListTest extends TestCase{
@@ -10,6 +11,8 @@ public class ToDoListTest extends TestCase{
 	private Task task1;
 	private Task task2;
 	private Task task3;
+	private LocalDate duedate1, duedate2, duedate3;
+	private int notification1, notification2, notification3;
 	private ToDoList todolist;
 	
 	public ToDoListTest() {
@@ -18,9 +21,15 @@ public class ToDoListTest extends TestCase{
 	@Before
 	 public void setUp() throws Exception{
 		//Initialise Test Fixtures
-		task1 = new Task("Desc 1");
-		task2 = new Task("Desc 2");
-		task3 = new Task("Desc 3");
+		duedate1 = LocalDate.of(2019,10,22);
+		duedate2 = LocalDate.of(2019,10,23);
+		duedate3 = LocalDate.of(2019,11,24);
+		notification1 = 0;
+		notification2 = 0;
+		notification3 = 5;
+		task1 = new Task("Desc 1", false, duedate1, notification1); //expect fail
+		task2 = new Task("Desc 2", false, duedate2, notification2); //expect notification
+		task3 = new Task("Desc 3", false, duedate3, notification3); //expect no notification
 		todolist = new ToDoList();
 	}
 	@After
@@ -29,15 +38,36 @@ public class ToDoListTest extends TestCase{
 		task1 = null;
 		task2 = null;
 		task3 = null;
+		duedate1 = null;
+		duedate2 = null;
+		duedate3 = null;
 		todolist = null;
 	}
 
 	@Test
 	public void testAddTask() {
+		String msg1 = "Task " + task1.getDescription() + ": Already overdue! ";
+		String msg2 = "Task " + task2.getDescription() + ": Due today. ";
+		String msg3 = "Task " + task3.getDescription() + ": Take your time! ";
+		
 		assertNotNull(todolist);
 		todolist.addTask(task1);
+
 		assertEquals(1, todolist.getAllTasks().size());
 		assertEquals(task1, todolist.getTask(task1.getDescription()));
+		assertEquals(duedate1, todolist.getTask(task1.getDescription()).getDueDate());
+		assertEquals(notification1, todolist.getTask(task1.getDescription()).getNotification());
+
+		System.out.println("task1 completed");
+		
+		assertEquals(msg1, todolist.getTask(task1.getDescription()).checkNotification(todolist.getTask(task1.getDescription())));
+		System.out.println("before add task2");
+
+		todolist.addTask(task2);
+		todolist.addTask(task3);
+
+		assertEquals(msg2, todolist.getTask(task2.getDescription()).checkNotification(todolist.getTask(task2.getDescription())));
+		assertEquals(msg3, todolist.getTask(task3.getDescription()).checkNotification(todolist.getTask(task3.getDescription())));
 	}
 	@Test
 	public void testgetStatus() {
